b212f757aa51c13c4ab998d8945b1da2c685fba3
Dustin Peterson <dustinr.peterson@gmail.com>
2019-01-24 20:41:14 -0600

    Working on bonus mission.

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -1,8 +1,6 @@
 package org.launchcode;
 
-import org.launchcode.comparators.AreaComparator;
-import org.launchcode.comparators.NameComparator;
-import org.launchcode.comparators.PopulationComparator;
+import org.launchcode.comparators.*;
 
 import java.util.ArrayList;
 
@@ -13,8 +11,12 @@ public class Main {
         ArrayList<City> cities = CityData.loadData();
 
         // TODO - Use different comparators here
-        PopulationComparator comparator = new PopulationComparator();
-        cities.sort(comparator);
+        //PopulationComparator comparator = new PopulationComparator();
+        //cities.sort(comparator);
+
+        CompoundComparator comparator = new CompoundComparator();
+        comparator.add(new StateComparator());
+        comparator.add(new PopulationComparator());
 
         printCities(cities);
 
--- src/org/launchcode/comparators/CompoundComparator.java
+++ src/org/launchcode/comparators/CompoundComparator.java
@@ -1,6 +1,38 @@
 package org.launchcode.comparators;
 
-import java.util.Comparator;
+import org.launchcode.City;
 
-public class CompoundComparator implements Comparator {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class CompoundComparator implements Comparator<City> {
+    List<Comparator<City>> comparators = new ArrayList<>();
+
+    @Override
+    public int compare(City o1, City o2) {
+
+        int compResult = 0;
+        for(int i = 0; i < comparators.size(); i++) {
+
+            Comparator comparator = comparators.get(i);
+            if(comparator.compare(o1, o2) == 0){
+
+                compResult = comparator.compare(o1, o2);
+                continue;
+            } else {
+                compResult =  comparator.compare(o1, o2);
+                break;
+            }
+
+
+        }
+        return compResult;
+    }
+
+
+    public void add(Comparator comparator) {
+        comparators.add(comparator);
+    }
+
 }
